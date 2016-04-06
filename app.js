80f2b7f892d7acb05856d3bf04315dad02937184
Dave Sroka <dsroka@wiredtriangle.com>
2016-04-06 01:03:47 -0400

    Add logging

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var led = new gpio(23, 'out');
 var button = new gpio(21, 'in', 'both');
 
 button.watch((err, state)=>{
+  console.log('button pushed');
   if (state==1){
     //turn led on
     led.writeSync(1);
