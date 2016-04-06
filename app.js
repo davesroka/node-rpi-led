cf0d449a1f91f914a23472bbca5b366232e40e45
Dave Sroka <dave@davesroka.net>
2016-04-06 00:07:28 -0400

    Added button press event

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var gpio = require('rpi-gpio');
 
 var pins = {
-    led1 : 18,
-    led2 : 23,
-    button : 21
+  led1: 18,
+  led2: 23,
+  button: 21
 }
 
-var lit = false;
+gpio.setup(pins.button, gpio.DIR_IN);
+gpio.setup(pins.led1, gpio.DIR_OUT);
 
-gpio.setup(pins.button, gpio.DIR_IN, readButton);
-gpio.setup(pins.led1, gpio.DIR_OUT); 
+gpio.on('change', (channel, value) => {
+  switch (channel) {
+    case pins.button:
+      gpio.write(pins.led1,1);
+  }
+});
 
-function readButton(){
-    gpio.read(pins.button, (err, value)=>{
-        console.log(`Button pushed value: ${value}`);
-        
-    });
+function readButton() {
+  gpio.read(pins.button, (err, value) => {
+    console.log(`Button pushed value: ${value}`);
+
+  });
 }
+
