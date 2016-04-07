aef14720e6e460606d294912aa3e257c61ae54a1
Dave Sroka <dave@davesroka.net>
2016-04-06 22:52:55 -0400

    Add test code

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ var gpio = require('onoff').Gpio;
 var led = new gpio(23, 'out');
 var button = new gpio(21, 'in', 'both');
 
-button.watch((err, state)=>{
-  console.log('button pushed');
-  if (state==1){
-    //turn led on
+// define the callback function
+function light(err, state) {
+  
+  // check the state of the button
+  // 1 == pressed, 0 == not pressed
+  if(state == 1) {
+    // turn LED on
     led.writeSync(1);
-  } else{
-    //tun led off
+  } else {
+    // turn LED off
     led.writeSync(0);
   }
-});
+  
+}
+ 
+// pass the callback function to the
+// as the first argument to watch()
+button.watch(light);
