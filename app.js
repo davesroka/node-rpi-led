f7a29ef2c87f4f56ddb17dafa154c8775b1ba1c1
Dave Sroka <dave@davesroka.net>
2016-04-15 23:48:07 -0400

    Update hello world text to text git push to remote server

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  res.send('Hi I changed this!');
 });
 
 app.listen(3000, function(){
