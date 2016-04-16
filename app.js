7cb035e64aebe325197b8ce203369a558b00484e
Dave Sroka <dave@davesroka.net>
2016-04-16 14:00:03 -0400

    Add new endpoints to turn lights on and off

--- app.js
+++ app.js
@@ -7,22 +7,35 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.listen(3001, function(){
+app.listen(3001, function () {
   console.log('App listening on port 3001!');
 });
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+app.get('/on', function (req, res) {
+  res.send('Lights on!');
+  red.writeSync(1);
+  green.writeSync(1);
+  blue.writeSync(1);
+});
+
+app.get('/off', function(req,res){
+   red.writeSync(0);
+   green.writeSync(0);
+   blue.writeSync(0)
+})
+
 // define the callback function
 function light(err, state) {
-  
+
   console.log('Button pushed');
-  
+
   // check the state of the button
   // 1 == pressed, 0 == not pressed
-  if(state == 1) {
+  if (state == 1) {
     // turn LED on
     red.writeSync(1);
     gree.writeSync(1);
@@ -31,11 +44,11 @@ function light(err, state) {
     // turn LED off
     red.writeSync(0);
     gree.writeSync(0);
-    blue.writeSync(0)
+    blue.writeSync(0);
   }
-  
+
 }
- 
+
 // pass the callback function to the
 // as the first argument to watch()
 button.watch(light);
